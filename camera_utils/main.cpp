da2a08f427e766e2358fe7406099373c62bd2ee7
wjb <bzyy223@163.com>
2023-04-05 12:43:21 +0800

    [feature] 完成双目相机图像采集类

--- camera_utils/main.cpp
+++ camera_utils/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include "src/dahen_camera.h"
+#include "src/stereo_camera.h"
 
-#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main() {
-	Camera* dh_cam = new DahenCamera((char*)"KG0210090307");
-	dh_cam->camInit();
+	// init camera
+	DahenCamera::libInit();
+	Camera* dh_cam_l = new DahenCamera((char*)"KG0210090307");
+	Camera* dh_cam_r = new DahenCamera((char*)"KG0210090308");
+	dh_cam_l->camInit();
+	dh_cam_r->camInit();
+	dh_cam_l->setExposure(5000);
+	dh_cam_r->setExposure(5000);
 
-	dh_cam->setExposure(5000);
-	auto img = dh_cam->getFrame();
-	dh_cam->close();
-	delete dh_cam;
-	cv::imwrite("D:/data/test/img.png", img);
+	auto stereo_cam = StereoCamera(dh_cam_l, dh_cam_r);
+	std::string left_path = "D:/PROJECT/work/camera_utils/data/stereo_imgs0/left/";
+	std::string right_path = "D:/PROJECT/work/camera_utils/data/stereo_imgs0/right/";
+	stereo_cam.captureImage(left_path, right_path);
+
+	// close camera
+	dh_cam_l->close();
+	dh_cam_r->close();
+	delete dh_cam_l;
+	delete dh_cam_r;
+	dh_cam_l = nullptr;
+	dh_cam_r = nullptr;
+	DahenCamera::libUinit();
+
+
 	return 0;
 }
--- camera_utils/src/stereo_camera.h
+++ camera_utils/src/stereo_camera.h
@@ -7,6 +7,8 @@ public:
 	StereoCamera(Camera* camera_left, Camera* camera_right) : cam_l(camera_left),cam_r(camera_right){}
 	~StereoCamera()=default;
 
+	void captureImage(std::string left_path, std::string right_path);
+
 private:
 	Camera* cam_l;
 	Camera* cam_r;
